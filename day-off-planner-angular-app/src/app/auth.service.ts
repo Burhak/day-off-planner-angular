fe6a08b93d48721bab2975dd5bd356f7117b9cf5
Lukáš Havrišák <altair213@gmail.com>
2019-10-10 11:08:48 +0200

    fixed swagger api library

--- day-off-planner-angular-app/src/app/auth.service.ts
+++ day-off-planner-angular-app/src/app/auth.service.ts
@@ -8,7 +8,7 @@ export class AuthService {
   constructor() { }
 
   getAccessToken(): string {
-    return 'Bearer ' +
-      'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhZG1pbkBhZG1pbi5jb20iLCJpYXQiOjE1NzA2Mzk5NjYsImV4cCI6MTU3MDcyNjM2Niwicm9sZXMiOlsiQURNSU4iXX0.yOoCBDSPti7qekceBX_kwAR9ougQ_xH-E1STSr72obhSuEF4cxg9e0zNa6YUynahYjMhWlwCODq3M2gRLKk-vQ'
+    // TODO: get token from session storage
+    return 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhZG1pbkBhZG1pbi5jb20iLCJpYXQiOjE1NzA2Mzk5NjYsImV4cCI6MTU3MDcyNjM2Niwicm9sZXMiOlsiQURNSU4iXX0.yOoCBDSPti7qekceBX_kwAR9ougQ_xH-E1STSr72obhSuEF4cxg9e0zNa6YUynahYjMhWlwCODq3M2gRLKk-vQ'
   }
 }
