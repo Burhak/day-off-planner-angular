917bac2a695af4f152f69bcff244e5e1ad5fce14
Burhak <altair213@gmail.com>
2019-10-14 09:01:14 +0000

    Generated code by SwaggerHub

--- src/app/api/api/user.service.ts
+++ src/app/api/api/user.service.ts
@@ -158,6 +158,51 @@ export class UserService {
     }
 
     /**
+     * Get currently logged user
+     * 
+     * @param observe set whether or not to return the data Observable as the body, response or events. defaults to returning the body.
+     * @param reportProgress flag to report request and response progress.
+     */
+    public getLoggedUser(observe?: 'body', reportProgress?: boolean): Observable<UserApiModel>;
+    public getLoggedUser(observe?: 'response', reportProgress?: boolean): Observable<HttpResponse<UserApiModel>>;
+    public getLoggedUser(observe?: 'events', reportProgress?: boolean): Observable<HttpEvent<UserApiModel>>;
+    public getLoggedUser(observe: any = 'body', reportProgress: boolean = false ): Observable<any> {
+
+        let headers = this.defaultHeaders;
+
+        // authentication (bearerAuth) required
+        // authentication (oAuthNoScopes) required
+        if (this.configuration.accessToken) {
+            const accessToken = typeof this.configuration.accessToken === 'function'
+                ? this.configuration.accessToken()
+                : this.configuration.accessToken;
+            headers = headers.set('Authorization', 'Bearer ' + accessToken);
+        }
+
+        // to determine the Accept header
+        let httpHeaderAccepts: string[] = [
+            'application/json'
+        ];
+        const httpHeaderAcceptSelected: string | undefined = this.configuration.selectHeaderAccept(httpHeaderAccepts);
+        if (httpHeaderAcceptSelected != undefined) {
+            headers = headers.set('Accept', httpHeaderAcceptSelected);
+        }
+
+        // to determine the Content-Type header
+        const consumes: string[] = [
+        ];
+
+        return this.httpClient.get<UserApiModel>(`${this.basePath}/user/me`,
+            {
+                withCredentials: this.configuration.withCredentials,
+                headers: headers,
+                observe: observe,
+                reportProgress: reportProgress
+            }
+        );
+    }
+
+    /**
      * Get user by ID
      * 
      * @param id User ID
