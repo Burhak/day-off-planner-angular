f0c8076e7fb22e4036b5d26733e42302e69aa325
Dávid Balogh <david.balogh@student.tuke.sk>
2019-10-16 23:26:10 +0200

    reset password done

--- src/app/reset-password/reset-password.component.ts
+++ src/app/reset-password/reset-password.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {Router} from '@angular/router';
+import {UserService} from '../api';
 
 @Component({
   selector: 'app-reset-password',
@@ -7,9 +10,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ResetPasswordComponent implements OnInit {
 
-  constructor() { }
+  private form: FormGroup;
+
+  constructor(private router: Router, private userService: UserService) { }
 
   ngOnInit() {
+    this.form = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email])
+    }, {updateOn: 'submit'});
+  }
+
+  goBack() {
+      this.router.navigate(['login']);
+  }
+
+  resetPassword(event) {
+    event.preventDefault();
+    if (!this.form.valid) {
+      console.log(this.form.valid);
+      return;
+    }
+    const newPassword: any = {
+      email: event.target.email.value
+    };
+
+    this.userService.resetPassword(newPassword).subscribe(
+      response => {
+        console.log(response);
+      }
+    );
   }
 
 }
