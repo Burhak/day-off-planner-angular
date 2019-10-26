176358f511ef0cc87b12098e897f4ef99e9d66a8
Dávid Balogh <david.balogh@student.tuke.sk>
2019-10-26 19:58:04 +0200

    showing user supervisor

--- src/app/component/user-profile/user-profile.component.ts
+++ src/app/component/user-profile/user-profile.component.ts
@@ -1,20 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnChanges, OnDestroy, OnInit, SimpleChanges} from '@angular/core';
 import {UserApiModel, UserService} from '../../api';
 import {UserInfoService} from '../../service/user-info.service';
+import {NavigationEnd, Router} from "@angular/router";
 
 @Component({
   selector: 'app-user-profile',
   templateUrl: './user-profile.component.html',
   styleUrls: ['./user-profile.component.css']
 })
-export class UserProfileComponent implements OnInit {
+export class UserProfileComponent implements OnInit{
 
-  private array: Array<UserApiModel> = [];
+  private userSupervisor: UserApiModel;
 
-  constructor(private userInfoService: UserInfoService, private userService: UserService) {
-    this.userService.getAllUsers().subscribe((user: UserApiModel[]) => {
-      this.array = user;
-    });
+  constructor(private userInfoService: UserInfoService, private userService: UserService, private router: Router) {
+    if (this.userInfoService.currentUser) {
+      if  (this.userInfoService.currentUser.supervisor) {
+        this.userService.getUserById(this.userInfoService.currentUser.supervisor).subscribe((user: UserApiModel) => {
+          this.userSupervisor = user;
+        });
+      }
+    }
   }
 
   ngOnInit() {
