faac314f24d7011cb0ebd735eba3c534cd6a843d
Lukáš Havrišák <lukas.havrisak@student.tuke.sk>
2019-11-06 19:43:52 +0100

    refactor error handling

--- src/app/service/error-handler.service.ts
+++ src/app/service/error-handler.service.ts
@@ -1,16 +1,18 @@
 import { Injectable, ErrorHandler, Injector, NgZone } from '@angular/core';
-import { Router } from '@angular/router';
 import { HttpErrorResponse } from '@angular/common/http';
-import { AuthService } from './auth.service';
+import { UserInfoService } from './user-info.service';
 import { Subject } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ErrorHandlerService implements ErrorHandler{
+export class ErrorHandlerService implements ErrorHandler {
 
   private errorMsg = new Subject<any>();
   public errorUpdate$ = this.errorMsg.asObservable();
+
+  private static readonly UNEXPECTED_ERROR = 'An unexpected error has occured';
 
   private updateMsg(msg: string) {
     this.errorMsg.next(msg);
@@ -19,36 +21,40 @@ export class ErrorHandlerService implements ErrorHandler{
   constructor(private injector: Injector) { }
 
   handleError(error: any) {
-    //const router = this.injector.get(Router);
-    const auth = this.injector.get(AuthService);
-    //const ngZone = this.injector.get(NgZone);
-
-    let errorMsg: string = 'An unexpected error has occured';
-
+    // handle common error
     if (error instanceof HttpErrorResponse) {
-
-      console.error('Backend returned status code: ', error.status);
-      console.error('Response body: ', error.message);
-
-      //Specific error status
-      switch (error.status) {
-        case 401: {
-          auth.removeToken();
-          errorMsg = 'Invalid credentials'
-          //ngZone.run(() => router.navigate(['error']));
-          break;
-        }
-        case 403: {
-          //errorMsg = '403: No admin rights'
-          break;
-        }
-      }
-
-    } else {
-      console.error('An error occurred: ', (<Error>error).message);
+      this.updateMsg(this.handleErrorResponse(error));
+      return;
     }
 
-    this.updateMsg(errorMsg);
-    //ngZone.run(() => router.navigate(['error']));
+    // handle error in promise
+    if (error.rejection && error.rejection instanceof HttpErrorResponse) {
+      this.updateMsg(this.handleErrorResponse(error.rejection));
+      return;
+    }
+
+    console.error(error);
+    this.updateMsg(ErrorHandlerService.UNEXPECTED_ERROR);
+  }
+
+  handleErrorResponse(error: HttpErrorResponse): string {
+    let errorMessage: string = (error.error && (error.error.message || error.error.error)) || ErrorHandlerService.UNEXPECTED_ERROR;
+    console.error('Status code: ', error.status);
+    console.error('Error: ', errorMessage);
+
+    //Specific error status
+    switch (error.status) {
+      case 401: {
+        const auth = this.injector.get(UserInfoService);
+        const router = this.injector.get(Router);
+        const ngZone = this.injector.get(NgZone);
+        auth.removeToken();
+        auth.removeUser();
+        ngZone.run(() => router.navigate(['login']));
+        return 'Session expired';
+      }
+    }
+
+    return ErrorHandlerService.UNEXPECTED_ERROR;
   }
 }
